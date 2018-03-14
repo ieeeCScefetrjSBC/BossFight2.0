43cfa91b7b6adc57d74e3f9a77f47d3827dbc050
Bernardo Simões do Nascimento <36808813+BernardoPLPSO@users.noreply.github.com>
2018-03-14 15:27:25 -0300

    Destruição basica de plataformas
    Adicionei um cubo como o boss, fiz ele lançar um raycast seguindo o jogador e se o raycast atingir uma plataforma ele destrói a plataforma
    
    Obs: Tem que implementar os 2 tipos de tiros (1 que destroi a plataforma outro que só da dano) e adicionar algum projetil visivel
    Co-Authored-By: Bernardo Simões do Nascimento <bernardoplpso@users.noreply.github.com>

---
+++ Assets/Scripts/Destruir.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruir : MonoBehaviour {
+
+    public void Vida_Plataforma(float dano)
+    {
+        if(dano == 20)
+        {
+            Kill();
+        }
+    }
+
+    // Use this for initialization
+    public void Kill()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Tiro_Boss.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Tiro_Boss : MonoBehaviour {
+    public float alcance = 100f;                   
+    public float dano_tiro1 = 10f;                 //Tiro que não destroi a plataforma 
+    public float dano_tiro2 = 20f;                 //Tiro mais forte que destroi a plataforma 
+    public float fireRate = 100f;                  //Quanto menor o fire rate mais tempo entre os tiros do boss 
+    public float tempoAtirar = 0f;
+    public Camera fpsCam;                          
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update () {
+        if (Time.time >= tempoAtirar)
+        {
+            tempoAtirar = Time.time + 1f / fireRate;
+            Atirar();
+        }
+
+    }
+
+    void Atirar ()
+    {
+        Vector3 posicao = transform.position;//inicio do raycast
+        Vector3 pos_player = posicao;//destino do raycast
+        if (player)
+        {
+           pos_player  = player.transform.position; 
+        }
+        Vector3 direcao = pos_player - posicao;//direcao do raycast
+        RaycastHit hit;
+        if (Physics.Raycast(posicao, direcao, out hit, alcance)){//se o raycast bater em algo                                          
+            Debug.Log(hit.transform.name);                       //registrar nome do objeto atingido
+            if(hit.transform.name != "Player")                   
+            {
+                Destruir plataforma = hit.transform.GetComponent<Destruir>();
+                if(plataforma != null)
+                {
+                    plataforma.Vida_Plataforma(dano_tiro2);
+                }
+            }
+        }
+    }
+}
