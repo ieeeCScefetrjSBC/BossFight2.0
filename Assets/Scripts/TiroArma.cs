206ba59428bc218480fee6c89eecb93754854b54
LucasPLTC <lucas.carvalho.pltc@gmail.com>
2018-03-16 01:26:27 -0300

    Animação de Tiro (Sem Partículas) e Tiro detectando alvo.

---
+++ Assets/Scripts/TiroArma.cs
@@ -0,0 +1,29 @@
+﻿
+using UnityEngine;
+
+public class TiroArma : MonoBehaviour {
+
+    public float dano = 10f;
+    public float range = 100f;
+
+    public Camera fpscam;
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GetComponent< Animation > ().Play("Tiro");
+
+            Atira();
+        }
+		
+	}
+
+    void Atira()
+    {
+        RaycastHit bang;
+       
+        if(Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out bang, range))
+        {
+            Debug.Log(bang.transform.name);
+        }
+    }
+}
