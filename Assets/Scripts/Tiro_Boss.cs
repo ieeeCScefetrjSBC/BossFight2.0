0ec446e65327d24ee33b403292d4eefc1c7e2e74
Lucaslsps <lucas.lsps@hotmail.com>
2018-03-16 14:01:31 -0300

    Arrumações

--- Assets/Scripts/Tiro_Boss.cs
+++ Assets/Scripts/Tiro_Boss.cs
@@ -1,14 +1,9 @@
 ﻿using UnityEngine;
 
 public class Tiro_Boss : MonoBehaviour {
-    private float alcance = 100f;                   
-    private float dano_tiro1 = 10f;                 //Tiro que não destroi a plataforma 
-    private float dano_tiro2 = 20f;                 //Tiro mais forte que destroi a plataforma 
     private float fireRate = 2;                  //Quanto menor o fire rate mais tempo entre os tiros do boss 
     private float tempoAtirar = 0f;
-    private int vida = 3;
     private GameObject player;
-    private Rigidbody tiroPrefab;
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
