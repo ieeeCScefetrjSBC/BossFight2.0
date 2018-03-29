787aeb03e8aa81c1ac1e6cb8869ec6233485828f
Lucas Silva Carvalho <lucas.scarvalho@eic.cefet-rj.br>
2018-03-29 01:17:06 -0300

    Colisões Funcionando
    .

---
+++ Assets/Scripts/OnCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class OnCollision : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Colidiu");
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Está colidindo");
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Saiu da colisão");
+    }
+}
