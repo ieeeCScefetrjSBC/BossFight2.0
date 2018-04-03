b606b94bab8ee2b6d6fa2d7b65138efa19752689
e213 <Antoniovsb25@gmail.com>
2018-04-03 13:48:45 -0300

    Sopro + Arte
    Primeira versão do sopro + umas cagadas do saiago

---
+++ Assets/Scripts/Mecânicas do Boss/Sopro.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sopro : MonoBehaviour {
+
+    private float Timer=0f;
+    private GameObject Player;
+    private GameObject Boss;
+    private Vector3 direction;
+
+	void Start ()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Boss = GameObject.FindGameObjectWithTag("Boss");
+	}
+	
+	void Update ()
+    {
+        direction = Boss.transform.position - Player.transform.position;
+        direction = direction.normalized;
+
+        Timer += Time.deltaTime;
+        if (Timer > 5f && Timer < 10f)
+            Player.GetComponent<Rigidbody>().AddForce(direction * 30);     
+        
+		
+	}
+}
