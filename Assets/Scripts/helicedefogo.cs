eff6deb6e22ecd73d8f793f66136bfaee0999f90
e213 <Antoniovsb25@gmail.com>
2018-04-07 11:38:46 -0300

    .
    .

---
+++ Assets/Scripts/helicedefogo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helicedefogo : MonoBehaviour {
+    public Transform helice;
+    public float vel;
+	void Start ()
+    {
+        
+	}
+	
+	void Update ()
+    {
+        helice.transform.Rotate(Vector3.up * Time.deltaTime * vel, Space.World);
+	}
+}
