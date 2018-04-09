1930a83c669f4648f59de6fa44c92bea8a6daf69
e213 <Antoniovsb25@gmail.com>
2018-04-09 20:06:07 -0300

    Sopro
    Sopro implementado detectando o chão

--- Assets/Scripts/Mecânicas do Boss/Sopro.cs
+++ Assets/Scripts/Mecânicas do Boss/Sopro.cs
@@ -4,32 +4,52 @@ using UnityEngine;
 
 public class Sopro : MonoBehaviour {
 
-    private float Timer=0f; // Timer placeholder
+    private float TimerGround=0f; // Timer que o player não sai do chão
+    private float TimerForce = 0f; // Tempo que a força é aplicada
+    private bool RequestForce = false;
     private GameObject Player; // Objeto do jogador na cena
     private GameObject Boss; // Objeto do boss na cena
+    private GameObject Grounder; //Objeto do chão
     private Vector3 direction; // Vetor direção da força
+    private float ForceMultiplier = 30f; // Multiplicador da força
 
-	void Start ()
+    void Start ()
     {
         Player = GameObject.FindGameObjectWithTag("Player"); // Encontra o player via tag
         Boss = GameObject.FindGameObjectWithTag("Boss"); // Encontra o Boss via tag
-	}
-	
-	void Update ()
+        Grounder = GameObject.FindWithTag("Grounder"); //Identifica o objeto Grounder
+    }
+
+    void Update()
     {
+        Debug.Log("TimerGround: " + TimerGround + ", TimerForce: " + TimerForce);
+
         direction = Boss.transform.position - Player.transform.position; //Define o ponto inicial como a posição do jogador e o final como a posição do boss
         direction = direction.normalized; //normaliza o vetor
-	
-        Timer += Time.deltaTime; // inicia o timer
-		
-	}
-	
-	void FixedUpdate()
-	{
-     
-		if (Timer > 5f && Timer < 10f) // Enquanto o timer estiver entre 5 e 10 segundos
-            Player.GetComponent<Rigidbody>().AddForce(direction * 20); // Aplica força de 30 no vetor direction
-        
-		
-	}
+
+        if (Grounder.GetComponent<Grounded>().getGrounded()) //Enquanto o player não sai do chão, inicia o timer 1
+            TimerGround += Time.deltaTime;
+        else
+            TimerGround = 0f;
+
+        if (TimerGround > 8f)
+        {
+            RequestForce = true;
+            TimerForce += Time.deltaTime;
+        }
+
+        if (TimerForce > 5f)
+        {
+            RequestForce = false;
+            TimerForce = 0f;
+        }
+
+        if (RequestForce) // Enquanto o timer estiver entre 5 e 10 segundos
+        {
+            Debug.Log("Carai ta voando");
+            Player.GetComponent<Mov>().setExtra_X(-ForceMultiplier);
+            Player.GetComponent<Mov>().setExtra_Z(-ForceMultiplier);
+        }
+    }
+
 }
