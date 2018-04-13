243367dd047a9ec31dab7ffd71a296f1cf150a7e
Krytz0 <brenocferreira2009@hotmail.com>
2018-04-12 22:18:39 -0300

    PILLARDAMAGE

---
+++ Assets/Scripts/VERIFICAR_PillarDamage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarDamage : MonoBehaviour
+{
+
+    int damagepillar = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void OncollisionEnter(Collision _collision)
+    {
+        if (_collision.Fire Pillar.tag == "damage by fire" ) {
+            Vida_Player -= damagepillar;
+            print(Vida_Player)
+          }
+    }
+}
