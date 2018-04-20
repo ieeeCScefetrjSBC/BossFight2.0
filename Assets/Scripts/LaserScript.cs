e546ecbb3f5fa3b02174a8d8c81bd7e584c7832b
Lara <lara.az13@gmail.com>
2018-04-20 18:29:36 -0300

    pode dar commit
    vlw

--- Assets/Scripts/LaserScript.cs
+++ Assets/Scripts/LaserScript.cs
@@ -30,13 +30,20 @@ public class LaserScript : MonoBehaviour
         while (Input.GetButton("Fire1")) //para disparos contínuos enquanto o botão está sendo apertado. Loopception
         {
             Ray ray = new Ray(transform.position, transform.forward); //ray inicia na ponta da arma e vai para frene, alinhado com a arma
+            RaycastHit Hit;
 
             Line.SetPosition(0, ray.origin); //começa no começo( ponta da arma)
+
+            if (Physics.Raycast(ray, out Hit, 100))
+                Line.SetPosition(1, Hit.point);
+            else
+                Line.SetPosition(1, ray.GetPoint(100));
+
             Line.SetPosition(1, ray.GetPoint(100)); //termina no final, ray 100 unidades adiante 
-            Debug.Log("laser");
+            
             yield return null; 
         }
         Line.enabled = false; //para a linha quando o botão deixa de ser apertado
-        Debug.Log("notlaser");
+        
     }
 }
