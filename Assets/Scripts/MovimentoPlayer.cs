bba334c1df82389f1759039472cd0564daafb8ff
Bernardo Simões do Nascimento <36808813+BernardoPLPSO@users.noreply.github.com>
2018-04-25 00:25:31 -0300

    Plataforma de Pulo 2.0
    Adicionei a plataforma de pulo para a nova movimentação (na minha opinião ta bem melhor agr) e mudei algumas coisas no funcionamento da movimentação (principalmente o fato de poder movimentar enquanto pula), tambem mudei a posição do player e tirei o rigidBody dele porque tava dando confilito c a movimentação

--- Assets/Scripts/MovimentoPlayer.cs
+++ Assets/Scripts/MovimentoPlayer.cs
@@ -11,10 +11,12 @@ public class MovimentoPlayer : MonoBehaviour
     float rotX;
     float rotY;
 
-    private float speed = 6.0F;
+    private float speed = 9.0F;
     private float jumpSpeed = 25.0F;
     private float gravity = 45.0F;
     private Vector3 moveDirection = Vector3.zero;
+    private float vertVel = 0f;//velocidade vertical do player (precisa disso para poder mexer enquanto está no pulo)
+    private bool platPulo = false;//variavel que detecta a colisão com a plataforma de pulo
 
     // Use this for initialization
     void Start()
@@ -26,43 +28,47 @@ public class MovimentoPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        
-                //Movimentação do Player
-                CharacterController controller = GetComponent<CharacterController>();
-                if (controller.isGrounded)
-                {
-                    if (Input.GetKey(KeyCode.LeftShift))
-                    {
-                        speed = 15.0F;
-                    }
-                    else { speed = 9.0F; }
+        //Movimentação do Player
+        CharacterController controller = GetComponent<CharacterController>();
 
-                    moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-                    moveDirection = transform.TransformDirection(moveDirection);
-                    moveDirection *= speed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 15.0f;
+        }
+        else
+        {
+            speed = 9.0F;
+        }
 
-                    if (Input.GetButton("Jump"))
-                        moveDirection.y = jumpSpeed;
-
-                }
-                moveDirection.y -= gravity * Time.deltaTime;
-                controller.Move(moveDirection * Time.deltaTime);
-
-            
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+        if (controller.isGrounded)
+        {
+            if (Input.GetButton("Jump") || platPulo == true)//faz o pulo do player quando o botão de pular for apertado, ou ele tenha colidido com a plataforma de pulo
+                vertVel = jumpSpeed;
+            else
+                vertVel = moveDirection.y;
+        }
+        vertVel -= gravity * Time.deltaTime;
+        moveDirection.y = vertVel;
+        controller.Move(moveDirection * Time.deltaTime);
     }
 
-    void OnControllerColliderHit(ControllerColliderHit hit) {
-        if(hit.gameObject.tag == "Plataforma"){
-            Debug.Log("PLATAFORMAAAAAAAA");
+    void OnControllerColliderHit(ControllerColliderHit hit) {//detecta colisão do player com a plataforma de pulo
+        if(hit.gameObject.tag == "PlataformaPulo")
+        {
+            Debug.Log("A");
+            jumpSpeed = 50f;
+            platPulo = true;
+            StartCoroutine(TempoDepoisDaPlataformaPulo());
         }
-        if(hit.gameObject.tag == "Chao"){
-            Debug.Log("CHAAAAAAAAAAAAAO");
-        }
-        if(hit.gameObject.tag == "PlataformaPulo"){
-            Debug.Log("PLATAFORMAAPULOOOOOO");
-            jumpSpeed = 10000000;
-        }
+    }
 
+    IEnumerator TempoDepoisDaPlataformaPulo()//esse metodo serve para reiniciar os valores de pulo depois que o player passa da plataforma de pulo
+    {
+        yield return new WaitForSeconds(2);
+        platPulo = false;
+        jumpSpeed = 25f;
     }
 }
