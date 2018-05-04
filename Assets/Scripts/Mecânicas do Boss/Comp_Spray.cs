5a3fecbe1d11cf48af58ff954ffbe85077a4b31c
Sombreiro <raphael.duarte33@hotmail.com>
2018-05-03 23:24:06 -0300

    Sprayed Shots
    Sprayed Shots refinado:
    Imprecisão baseada na distância do jogador;
    Taxa de convergência ajustada para maior dificuldade;

--- Assets/Scripts/Mecânicas do Boss/Comp_Spray.cs
+++ Assets/Scripts/Mecânicas do Boss/Comp_Spray.cs
@@ -14,24 +14,26 @@ public class Comp_Spray : MonoBehaviour {
     private Vector3 Center; // Vetor que guarda a posição do player no momento que o tiro é lançado
     private Vector3 Direction; // Direção do movimento
     private GameObject Player; // Objeto do Jogador
+    [SerializeField]
+    private float Convergence=0.7f;
    
 	void Start () {
-        Origin = transform.position;
         Player = GameObject.FindGameObjectWithTag("Player"); // Define GameObject Player
+        Origin = transform.position;// Posição do Objeto       
+        Center = Player.transform.position;// Posição do player
         Teta = Random.Range(0, Mathf.PI * 2f); // Ângulo horizontal aleatório
         Phi = Random.Range(0, Mathf.PI);// Ângulo vertical aleatório
-        Radius = Random.Range(0, 30f);// Distância do player aleatória
+        Radius = Random.Range(0, ((Center-Origin).magnitude/2f)-6);// Distância do player aleatória
         Finale.x = Player.transform.position.x + Radius * Mathf.Cos(Teta) * Mathf.Sin(Phi); // Posição X do Jogador + Coordenada Esférica
         Finale.y = Player.transform.position.y + Radius * Mathf.Sin(Teta) * Mathf.Sin(Phi);// Posição Y do Jogador + Coordenada Esférica
-        Finale.z = Player.transform.position.z + Radius * Mathf.Cos(Phi);// Posição Z do Jogador + Coordenada Esférica
-        Center = Player.transform.position;
+        Finale.z = Player.transform.position.z + Radius * Mathf.Cos(Phi);// Posição Z do Jogador + Coordenada Esférica        
         Direction = (Finale-transform.position).normalized;// Direção do movimento definida como ponto de destino - ponto de origem
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if ((Center - Origin).magnitude > (transform.position - Origin).magnitude)
-            transform.Translate((Direction+(Center-transform.position).normalized/1f)*Time.deltaTime*Speed); // Muda a posição do objeto no vetor Direction e converge para a posição do player no momento do lançamento
+        if ((Center - Origin).magnitude > (transform.position - Origin).magnitude)// Caso o tiro ainda não tenha ultrapassado o player
+            transform.Translate((Direction+(Center-transform.position).normalized/Convergence)*Time.deltaTime*Speed); // Muda a posição do objeto no vetor Direction e converge para a posição do player no momento do lançamento
         else
             transform.Translate(Direction * Time.deltaTime * Speed); // Muda a posição do objeto no vetor Direction, usando escala temporal e com velocidade "Speed"
     }
