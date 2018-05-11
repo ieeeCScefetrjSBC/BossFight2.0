8b02a895cbf622a35ead80a125825bc99b52d0b1
phtriplox <pedrohfcarneiro1@gmail.com>
2018-05-10 22:31:29 -0300

    Começando a helice de vento

--- Assets/Scripts/Mecânicas do Boss/HeliceDeVento.cs
+++ Assets/Scripts/Mecânicas do Boss/HeliceDeVento.cs
@@ -2,27 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HeliceDeFogo : MonoBehaviour {
+public class HeliceDeVento : MonoBehaviour {
+
     private GameObject Player;
-    private Vector3 Mov_Direção;
-    public float Mov_Vel;
-    public float Mov_Vel_Perto;
-    public float Vel_Rotx, Vel_Roty, Vel_Rotz;
+    private Vector3 Mov_Direção; //Vetor direção do movimento
+    private Vector3 direction; // Vetor direção da força
+    [SerializeField] private float Mov_Vel;
+    [SerializeField] private float Mov_Vel_Perto;
+    [SerializeField] private float Vel_Rotx, Vel_Roty, Vel_Rotz;
+    [SerializeField] private float ForceMultiplier; // Multiplicador da força dependente da distância entre player e helice
+    [SerializeField] private float DistanciaDeAtivação; // Distância em que o sopro da hélice é ativado.
     private Vida_Player Vida_Player;
 
     void Start()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         Vida_Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Vida_Player>();
+        ForceMultiplier = 0;
     }
 
     void Update()
     {
-        Mov_Direção = new Vector3(Player.transform.position.x, Player.transform.position.y + 1.5f, Player.transform.position.z) - transform.position;
+        // MOVIMENTO DA HELICE!!!
+        Mov_Direção = new Vector3(Player.transform.position.x, Player.transform.position.y + 1.5f, Player.transform.position.z) - transform.position;  //Direção de movimento da helice
         transform.Rotate(Vector3.right * Time.deltaTime * Vel_Rotx, Space.World);   // rotação em x
         transform.Rotate(Vector3.up * Time.deltaTime * Vel_Roty, Space.World);   // rotação em y
         transform.Rotate(Vector3.forward * Time.deltaTime * Vel_Rotz, Space.World);   // rotação em z
-        if ((Player.transform.position - transform.position).magnitude >= 4)
+        if ((Player.transform.position - transform.position).magnitude >= 4)     // Verifica se está perto ou distante do player
         {
             transform.Translate(Mov_Direção.normalized * Mov_Vel * Time.deltaTime, Space.World);
             Debug.Log("Fugiu");
@@ -33,7 +39,20 @@ public class HeliceDeFogo : MonoBehaviour {
             Debug.Log("DANOUSE");
             //Vida_Player.danoPlayer(1);
         }
+
+        // SOPRO DA HELICE DE VENTO
+        direction = transform.position - Player.transform.position;  // Direção do player à helice
+        if (direction.magnitude <= DistanciaDeAtivação)
+        {
+            Player.GetComponent<CharacterController>().Move(direction.normalized * ForceMultiplier);
+            Debug.Log("puxando");
+        }
     }
+
+
+
+
+
     void OnParticleCollision(GameObject other)
     {
         Rigidbody teste = other.GetComponent<Rigidbody>();
@@ -44,4 +63,3 @@ public class HeliceDeFogo : MonoBehaviour {
         }
     }
 }
-
