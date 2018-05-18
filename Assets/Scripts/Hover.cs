6754c130c88fe93a063a0dbbade26e129949759b
kerbeks <petropolis.dudu@gmail.com>
2018-05-18 00:03:36 -0300

    hover das mascaras
    Adicionado efeito de hover no boss

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour {
+
+ public float altitude;
+ public float verticalSpeed;
+ public float amplitude;
+ 
+ private Vector3 tempPosition;
+
+	// Use this for initialization
+	void Start () {
+		tempPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+  		tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed)* amplitude;
+  		transform.position = tempPosition + new Vector3 (0,altitude,0);
+	}
+}
