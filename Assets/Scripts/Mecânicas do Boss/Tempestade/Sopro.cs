a616512bae1e9d7b1a126cf2f1613d25e3ca6faa
kerbeks <petropolis.dudu@gmail.com>
2018-06-01 20:45:26 -0300

    Revert "Codigo do sopro"
    This reverts commit 8543090af16670dea42e0852c9a295d030d8c9da.

--- Assets/Scripts/Mecânicas do Boss/Tempestade/Sopro.cs
+++ Assets/Scripts/Mecânicas do Boss/Tempestade/Sopro.cs
@@ -10,21 +10,17 @@ public class Sopro : MonoBehaviour {
     private GameObject Player; // Objeto do jogador na cena
     private GameObject Boss; // Objeto do boss na cena
     private GameObject Grounder; //Objeto do chão
-    private GameObject Tell; //Objeto que indica visualmente o sopro
     private Vector3 direction; // Vetor direção da força
     private float ForceMultiplier = 0.1f; // Multiplicador da força
     private float DuracaoSopro = 2f;
     private float TempoAteSopro = 8f;
     private bool SoproAtivado = false; // Luciano esteve aqui muahahaha
-    private bool Sopro_Tell = false;
 
     void Start ()
     {
         Player = GameObject.FindGameObjectWithTag("Player"); // Encontra o player via tag
         Boss = GameObject.FindGameObjectWithTag("Boss"); // Encontra o Boss via tag
         Grounder = GameObject.FindWithTag("Grounder"); //Identifica o objeto Grounder
-        Tell = GameObject.FindGameObjectWithTag("Sopro");// Define qual é o objeto sopro
-        Tell.SetActive(false);
     }
 
     void Update()
@@ -63,15 +59,6 @@ public class Sopro : MonoBehaviour {
 
             Player.GetComponent<Rigidbody>().AddForce(direction.normalized * ForceMultiplier, ForceMode.Force);
             //Player.GetComponent<Mov>().setExtra_Z(-ForceMultiplier);
-            Debug.Log("glub glub");
-           /* if (Sopro_Tell = false)
-            {
-                Sopro_Tell = true;
-                Tell.SetActive(true);
-            }
-            Sopro_Tell = false;
-            Tell.SetActive(false);*/
-
         }
     }
 
