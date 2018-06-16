f2fd04181890af949213598a86871ec12e0ef9d9
phtriplox <pedrohfcarneiro1@gmail.com>
2018-06-16 00:02:24 -0300

    Tiro Blaster quase pronto
    Falta só botar pra quando colidir com as máscaras dar dano, botar pra ativar as particulas e parar de colidir com outros tiros.

---
+++ Assets/Scripts/Player/Blaster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blaster : MonoBehaviour {
+
+    private Camera MainCamera;
+    private GameObject Ponta_Arma;
+    private GameObject Tiro;
+    private Vector3 Direction;
+
+	void Start () {
+        Tiro = GameObject.FindGameObjectWithTag("Tiro_Player");
+        Ponta_Arma = GameObject.FindGameObjectWithTag("Arma");
+        MainCamera = Camera.main;
+	}
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Atira();
+        }
+
+    }
+
+    void Atira()
+    {
+        RaycastHit Hit;
+
+
+        Debug.Log("OPA");
+        if (Physics.Raycast(MainCamera.transform.position, MainCamera.transform.forward))
+        {
+            GameObject Projetil = (GameObject)GameObject.Instantiate(Tiro, Ponta_Arma.transform.position + Ponta_Arma.transform.forward.normalized * 2, Quaternion.Euler(MainCamera.transform.rotation.eulerAngles + new Vector3(90,0,0)));
+            Projetil.GetComponent<Tiro_Blaster>().set_Direction(MainCamera.transform.forward);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Tiro_Blaster.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tiro_Blaster : MonoBehaviour {
+
+    Vector3 Direction;
+    [SerializeField] private float Tiro_Speed;
+
+	void Start () {
+        this.gameObject.GetComponent<Rigidbody>().velocity = Direction.normalized * Tiro_Speed;
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+        Debug.Log("Ativa particula");
+    }
+
+    public void set_Direction(Vector3 direction)
+    {
+        Direction = direction;
+        return;
+    }
+}
