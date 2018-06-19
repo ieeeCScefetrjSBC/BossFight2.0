48327946b127c4decde185a803266550d94323a0
LucasJean <lucasjeanv@gmail.com>
2018-06-18 21:32:42 -0300

    tiro com som e tempo de cooldown

--- Assets/Scripts/Player/Blaster.cs
+++ Assets/Scripts/Player/Blaster.cs
@@ -1,26 +1,46 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Blaster : MonoBehaviour {
 
-    private Camera MainCamera;
+    public AudioSource shotSound;
+    public float coolDownTime = 0.1f;
+    public float lightTime = 0.1f;
+
     private GameObject Ponta_Arma;
     private GameObject Tiro;
+    private Camera MainCamera;
+    private Light shotLight;
+
     private Vector3 Direction;
+    private float lastShotTime = 0f;
+    private float timeSinceShot;
 
-    private GameObject Luz;
-
-	void Start () {
-        Tiro = GameObject.FindGameObjectWithTag("Tiro_Player");
+	void Start ()
+    {
         Ponta_Arma = GameObject.FindGameObjectWithTag("Arma");
+        Tiro = GameObject.FindGameObjectWithTag("Tiro_Player");
         MainCamera = Camera.main;
+        shotLight = Ponta_Arma.GetComponent<Light>();
 
+        shotLight.enabled = false;
+        timeSinceShot = -coolDownTime;
 	}
 	
-	void Update () {
-        if (Input.GetMouseButtonDown(0))
+	void Update ()
+    {
+        timeSinceShot = Time.time - lastShotTime;
+
+        if (timeSinceShot >= lightTime)
+            shotLight.enabled = false;
+
+        if (Input.GetMouseButtonDown(0) && timeSinceShot >= coolDownTime)
         {
+            lastShotTime = Time.time;
+            shotLight.enabled = true;
+            shotSound.Play();
+
             Atira();
         }
 
@@ -29,12 +49,12 @@ public class Blaster : MonoBehaviour {
     void Atira()
     {
         RaycastHit Hit;
-
-
+        
         Debug.Log("OPA");
         if (Physics.Raycast(MainCamera.transform.position, MainCamera.transform.forward))
         {
-            GameObject Projetil = (GameObject)GameObject.Instantiate(Tiro, Ponta_Arma.transform.position + Ponta_Arma.transform.forward.normalized * 2, Quaternion.Euler(MainCamera.transform.rotation.eulerAngles + new Vector3(90,0,0)));
+            GameObject Projetil = (GameObject)GameObject.Instantiate(Tiro, Ponta_Arma.transform.position + Ponta_Arma.transform.forward.normalized * 2,
+                                   Quaternion.Euler(MainCamera.transform.rotation.eulerAngles + new Vector3(90,0,0)));
             Projetil.GetComponent<Tiro_Blaster>().enabled = true;
             Projetil.GetComponent<Tiro_Blaster>().set_Direction(MainCamera.transform.forward);
         }
