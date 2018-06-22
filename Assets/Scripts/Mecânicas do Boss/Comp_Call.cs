3a56c18c1d07d32dd63d74ae6e53321e0d299cdc
PC 213 A <ramocefetrj@gmail.com>
2018-06-22 19:23:37 -0300

    Sopro reestruturado no comp_call

--- Assets/Scripts/Mecânicas do Boss/Comp_Call.cs
+++ Assets/Scripts/Mecânicas do Boss/Comp_Call.cs
@@ -9,17 +9,23 @@ public class Comp_Call : MonoBehaviour {
     private Comp_Bomba Comp_Bomba;// Script referente ao Comp_Bomba
     private Tiro_Boss Tiro_Boss;// Script referente ao Tiro_Boss
 	private Comp_Helice Comp_Helice; // Script referente à Comp_Helice
+    private Sopro Comp_Sopro; // Script referente ao sopro
+
     private delegate void myMechanics(int Pattern);//
-    private myMechanics[] Mechanics= new myMechanics[2];// Variável que guarda métodos
+    private myMechanics[] Mechanics= new myMechanics[3];// Variável que guarda métodos
     private int Contador;// Indica a posição da mecânica a ser ativada e qual padrão será usado
     public int[] Index_Mechanics;// Posição da mecânica no Array Mechanics
     public int[] Module;// Padrão da mecânica(Caso tenha apenas um padrão, colocar 0)
+
     void Start () {
         Comp_Bomba = this.gameObject.GetComponent<Comp_Bomba>();// Define quem é Comp_Bomba
         Tiro_Boss = this.gameObject.GetComponent<Tiro_Boss>();// Define quem é Tiro_Boss
 		Comp_Helice = this.gameObject.GetComponent<Comp_Helice>(); // Define quem é Comp_Helice
+        Comp_Sopro = this.gameObject.GetComponent<Sopro>(); // Define que é Cómp_Sopro
+
         Mechanics[0] = Call_Bomba;// Espaço 0 é a mecânica de bomba!
         Mechanics[1] = Call_Helice; // Espaço 1 é a mecânica de helice!
+        Mechanics[2] = Call_Sopro; // Espaço 2 é a mecânica de sopro!
     }
 	
 	// Update is called once per frame
@@ -45,7 +51,11 @@ public class Comp_Call : MonoBehaviour {
     }
     private void Call_Helice(int Pattern) // Define o padrão de helice
     {
-        Comp_Helice.Call(Pattern);
+        Comp_Helice.Call(Pattern); // Passa o padrão de sopro
+    }
+    private void Call_Sopro(int Pattern) // Define o padrão de sopro
+    {
+        Comp_Sopro.Call(Pattern); // Passa o padrão de sopro
     }
     public void setTempo(float Tempo)// Modifica o tempo para a próxima mecânica
     {
