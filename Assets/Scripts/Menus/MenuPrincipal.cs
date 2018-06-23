676944c2afaf54f97f4a135d6fc15b1cd96f1569
Gabsart <garthurcabral@gmail.com>
2018-06-23 12:56:23 -0300

    Menu Principal com tutorial

--- Assets/Scripts/Menus/MenuPrincipal.cs
+++ Assets/Scripts/Menus/MenuPrincipal.cs
@@ -2,25 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuPrincipal : MonoBehaviour {
 
+	private Image tutorial;
+	private GameObject menuPrincipal;
+	private bool tutorialAtivado = false;
+
     void Start()
     {
-
+    	tutorial = GameObject.FindGameObjectWithTag("tutorial").GetComponent<Image>();
+    	tutorial.enabled = false;
+    	menuPrincipal = GameObject.FindGameObjectWithTag("MenuPrincipal");
+    	menuPrincipal.SetActive(true);
     }
 
 
     void Update()
     {
-
+    	if(tutorialAtivado) 
+    	{
+    		if(Input.GetMouseButtonDown(0)) 
+    		{
+    			SceneManager.LoadScene("GD Room");
+    			Time.timeScale = 1;
+    		}
+    	}
     }
 
     public void Jogar()
     {
         Debug.Log("Jogar");
-        SceneManager.LoadScene("GD Room");
-        Time.timeScale = 1;
+        menuPrincipal.SetActive(false);
+        tutorial.enabled = true;
+        tutorialAtivado = true;
     }
 
     public void Sair()
